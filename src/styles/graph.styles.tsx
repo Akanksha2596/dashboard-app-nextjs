a09bdc9889d8d5aa10dfae8e7a256ff9bf564020
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-23 18:48:42 +0530

    Working on Graph UI

---
+++ src/styles/graph.styles.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const GraphContainer = styled.div`
+  width: 1208px;
+  height: 292px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  gap: 10px;
+  // border: 1px solid pink;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  margin: auto;
+`;
+
+export const GraphHeading = styled.h3`
+  width: 1208px;
+  height: 86px;
+  padding: 32px, 32px, 28px, 32px;
+  gap: 16px;
+  font-family: Strawford;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 26px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const GraphImage = styled.img`
+  width: 1208px;
+  height: 108px;
+  gap: 10px;
+`;
+
+export const GraphReading = styled.h1`
+  display: flex;
+  width: 1208px;
+  height: 82px;
+  padding: 0px, 32px, 32px, 32px;
+  gap: 16px;
+  font-family: Strawford;
+  font-size: 42px;
+  font-weight: 500;
+  line-height: 50px;
+  letter-spacing: 0em;
+  text-align: center;
+`;
