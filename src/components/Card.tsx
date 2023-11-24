abb8dfe49553733bc555006dda9bbfff764abf6a
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-24 15:39:39 +0530

    updating card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,15 +2,11 @@ import styled from "styled-components";
 import React from "react";
 import ProductSingleCard from "./ProductSingleCard";
 import { CardContainer } from "@/styles/card.styles";
-import PostSingleCard from "./PostSingleCard";
-import StoreSingleCard from "./StoreSingleCard";
 
 const Card = () => {
   return (
     <CardContainer>
-      <ProductSingleCard HeadingText="Top Products" ArrowIcon=">" />
-      <PostSingleCard HeadingText="Top Posts" ArrowIcon=">" />
-      <StoreSingleCard HeadingText="Top Stores" ArrowIcon=">" />
+      <ProductSingleCard />
     </CardContainer>
   );
 };
