127f87aee6d0bcce3f1fd088aff52ef87e8fe09c
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-27 18:50:08 +0530

    rendering detailed page

--- src/pages/topProducts.tsx
+++ src/pages/topProducts.tsx
@@ -1,17 +1,3 @@
-// import React from "react";
-// import ButtonFilter from "@/components/ButtonFilter";
-// import { Item } from "@/components/Item";
-// import topProductData from "../../TopProductsData.json";
-// const topProducts = () => {
-//   return (
-//     <>
-//       <ButtonFilter />
-//       <Item topProductData={topProductData} HeadingText="Top Products" />
-//     </>
-//   );
-// };
-
-// export default topProducts;
 import React from "react";
 import styled from "styled-components";
 import Tabs from "@/components/Tabs";
@@ -20,49 +6,55 @@ import topStoresData from "../../TopStoresData.json";
 import topPostsData from "../../TopPostsData.json";
 import ButtonFilter from "@/components/ButtonFilter";
 import { Item } from "@/components/Item";
-
-const PageContainer = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-`;
+import { Wrapper } from "@/styles/card.styles";
+import { RootWrapper } from "@/styles/button.styles";
 
 const Content = styled.div`
   margin-top: 20px;
 `;
 
 const Page: React.FC = () => {
-  const tabs = ["Top Products ", "Top Posts", "Tap Stores"];
+  const tabs = ["Top Products", "Top Posts", "Top Stores"];
 
   return (
-    <PageContainer>
-      <Tabs tabs={tabs} defaultTab={tabs[0]}>
+    <>
+      <Tabs tabs={tabs} defaultTab={"Top Products"}>
         {(activeTab) => (
           <Content>
             {activeTab === "Top Products" && (
               <>
                 <ButtonFilter />
-                <Item
-                  topProductData={topProductsData}
-                  HeadingText="Top Posts"
-                />
+                <Wrapper>
+                  <Item
+                    topProductData={topProductsData}
+                    HeadingText="Top Products"
+                  />
+                </Wrapper>
               </>
             )}
             {activeTab === "Top Posts" && (
               <>
                 <ButtonFilter />
-                <Item topProductData={topPostsData} HeadingText="Top Posts" />
+                <Wrapper>
+                  <Item topProductData={topPostsData} HeadingText="Top Posts" />
+                </Wrapper>
               </>
             )}
-            {activeTab === "Tap Stores" && (
+            {activeTab === "Top Stores" && (
               <>
                 <ButtonFilter />
-                <Item topProductData={topStoresData} HeadingText="Top Stores" />
+                <Wrapper>
+                  <Item
+                    topProductData={topStoresData}
+                    HeadingText="Top Stores"
+                  />
+                </Wrapper>
               </>
             )}
           </Content>
         )}
       </Tabs>
-    </PageContainer>
+    </>
   );
 };
 
