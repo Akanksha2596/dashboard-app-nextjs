dba22746cc80012bdd102f938d08d9aca78287e2
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-27 18:49:24 +0530

    Tabs Component

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,45 +1,62 @@
- import React, { useState, ReactNode } from 'react';
-import styled from 'styled-components';
+import React, { useState, ReactNode } from "react";
+import styled from "styled-components";
+import { RootWrapper, TabWrapper } from "@/styles/button.styles";
 
-const TabsContainer = styled.div`
-  margin-bottom: 20px;
-`;
-
-interface TabButtonProps {
+type TabButtonProps = {
   isActive: boolean;
-}
+};
 
 const TabButton = styled.button<TabButtonProps>`
-  font-weight: ${(props) => (props.isActive ? 'bold' : 'normal')};
+  font-weight: ${(props) => (props.isActive ? "bold" : "normal")};
   margin-right: 10px;
+  display: flex;
+  margin-right: 10px;
+  align-items: center;
+  width: 89px;
+  height: 39px;
+  padding: 0px 14px;
+  border-radius: 20px;
+  border: 1px solid #dddddd;
+  color: #9d9d9d;
+  background-color: white;
+  white-space: nowrap;
+
+  &:hover {
+    border: 1px solid black;
+    color: black;
+    cursor: pointer;
+  }
 `;
 
-interface TabsProps {
+type TabsProps = {
   tabs: string[];
   defaultTab: string;
   children: (activeTab: string) => ReactNode;
-}
+};
 
 const Tabs: React.FC<TabsProps> = ({ tabs, defaultTab, children }) => {
-  const [activeTab, setActiveTab] = useState("Top Products");
+  const [activeTab, setActiveTab] = useState(defaultTab);
 
   const handleTabClick = (tab: string) => {
+    console.log("Clicked tab:", tab);
     setActiveTab(tab);
   };
 
   return (
-    <TabsContainer>
-      {tabs.map((tab) => (
-        <TabButton
-          key={tab}
-          isActive={activeTab === tab}
-          onClick={() => handleTabClick(tab)}
-        >
-          {tab}
-        </TabButton>
-      ))}
+    <RootWrapper>
+      <TabWrapper>
+        {tabs.map((tab) => (
+          <TabButton
+            key={tab}
+            isActive={activeTab === tab}
+            onClick={() => handleTabClick(tab)}
+          >
+            {tab}
+          </TabButton>
+        ))}
+      </TabWrapper>
       <div>{children(activeTab)}</div>
-    </TabsContainer>
+    </RootWrapper>
   );
 };
 
