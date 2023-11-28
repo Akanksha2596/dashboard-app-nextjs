95abf36963580eb01075c5238719d2a79a01d7cc
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-28 19:07:58 +0530

    updated global file

---
+++ src/styles/Global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    body{
+        background-color: #FFFFFF;
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+        font-size: 16px;
+        
+    }
+
+    @media only screen and (max-width: 920px) {
+    font-size: 14px;
+  }
+`;
