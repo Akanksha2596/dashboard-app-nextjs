571759a8dc711a64356f89c6546c32a73f644ca7
Akanksha2596 <akanksha.yadav@brainvire.com>
2023-11-28 18:44:00 +0530

    importing styles for font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+// import { Global } from "@/styles/Global";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+
+export const myFont = localFont({
+  src: "../../public/strawford-regular-webfont.woff2",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={myFont.className}>
+      {/* <Global /> */}
+      <Component {...pageProps} />
+    </main>
+  );
 }
